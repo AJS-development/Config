417886142a3ef0892663202b9132603d2c5da254
Andrew54757 <andrews54757@gmail.com>
2016-08-15 16:38:16 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 "use strict"
-
+const parser = require('./parser.js')
+const stringify = require ('./stringify.js')
 
 module.exports = class ConfigLoader {
 constructor(configs) {
 this.configs = configs
 }
-
-
-
+parse(string) {
+  return parser(string)
+  
+}
+stringify(config) {
+  
+  return stringify(config)
+}
+stringifyConcat(config,add) {
+  
+  
+}
 
 
 

1b983525f52fe4846abf53ee39372e7faa5e06be
Andrew54757 <andrews54757@gmail.com>
2016-08-15 17:02:02 +0000

    Update parser.js

--- parser.js
+++ parser.js
@@ -1,15 +1,48 @@
 "use strict"
 
-module.exports = function(string) {
+module.exports = function(string,configs) {
+  var results = [];
 var split = string.split("\n")
-for (var i in split) {
-var con = split[i]
-if (con.charAt(0) == "#") continue;
+split.forEach((con)=>{
+  if (con.charAt(0) == "#") return;
+var c = con.split(" ")
+var config = c[0]
+if (!configs[config]) return;
 
-
+if (configs[config] == "Jstring") {
+if (!c[1]) return
+  var name = c.slice(1, c.length).join(' ');
+  results[config] == name
+} else if (configs[config] == "Ostring") {
+  if (!c[1]) return
+var r = [];
+  for (var j in c) {
+    if (j == 0) continue
+    r[j - 1] = c[j]
+    
+  }
+  results[config] = r;
+} else if (configs[config] == "string") {
+  if (!c[1]) return
+  results[config] = c[1].toString()
+} else if (configs[config] == "int") {
+  if (!c[1]) return results[config] = 0
+  results[config] = parseInt(c[1])
+  
+  
+} else if (configs[config] == "Oint") {
+  if (!c[1]) return results[config] = [0]
+  var r = [];
+  for (var j in c) {
+    if (j == 0) continue
+    r[j - 1] = parseInt(c[j])
+    
+  }
+  results[config] = r;
+  
 }
-
-
-
-
+  
+  
+})
+return results;
 }

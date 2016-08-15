45038c505f167c6f59c7917318b1de251ee702a1
Andrew54757 <andrews54757@gmail.com>
2016-08-15 16:41:18 +0000

    Create parser.js

---
+++ parser.js
@@ -0,0 +1,15 @@
+"use strict"
+
+module.exports = function(string) {
+var split = string.split("\n")
+for (var i in split) {
+var con = split[i]
+if (con.charAt(0) == "#") continue;
+
+
+}
+
+
+
+
+}

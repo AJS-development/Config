e61d09131f16589d89702ebc85cd16443d594c9f
Andrew54757 <andrews54757@gmail.com>
2016-08-15 20:54:04 +0000

    Update stringify.js

--- stringify.js
+++ stringify.js
@@ -1,13 +1,28 @@
 module.exports = function(config,configs) {
+  results = "";
 config.forEach((con,conind)=>{
 var type = configs[conind]
-
-
+results += conind;
+if (type == "Ostring" || type == "Oint") {
+  
+  con.forEach((c)=>{
+      results += " " + c
+    
+  })
+  
+  
+} else if (type == "string" || type == "Jstring" || type == "int") {
+  
+  results += " " + con;
+  
+  
+}
+results += "\n";
 
 
 });
 
 
-
+return results;
 
 }
